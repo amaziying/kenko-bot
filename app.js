a278c0a7f10b4ea3207ad7aec41e29fde7194e44
Ziying Zhang <ziyingzhanginc@gmail.com>
2017-11-19 13:24:48 -0500

    Send hi message back

--- app.js
+++ app.js
@@ -382,7 +382,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendHiMessage(senderID);
 }
 
 /*
